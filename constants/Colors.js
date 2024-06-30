0295f9543766a151257f3288f7fd6f012abb8fab
UserNameWasTakenAlreadyy <97379279+UserNameWasTakenAlreadyy@users.noreply.github.com>
2024-06-30 19:47:09 +0800

    Add files via upload

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,7 +1,13 @@
 const Colors = {
     primaryBackground: '#fddddf',
     fontColor: '#103B59',
-    primaryComplement: '#C3FFE6'
+    primaryComplement: '#C3FFE6',
+    add: '#3cefa4',
+    logOut: '#fa5d67',
+    menuMerchant: '#e0fdf3',
+    menuMerchantBorder: '#000000',
+    set: '#4fa3e4',
+    dismiss: '#e76852'
 }
 
 export default Colors;
